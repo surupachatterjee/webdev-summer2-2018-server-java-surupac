e4890da2f7e00185d6d9075275c194355bd34245
chatterjeesurupa <chatterjee.su@husky.neu.edu>
2018-07-13 18:31:15 -0400

    added field mandate on login

--- src/main/webapp/jquery/components/login/login.controller.client.js
+++ src/main/webapp/jquery/components/login/login.controller.client.js
@@ -1,38 +1,34 @@
 (function () {
 
     $(main)
-
     var userService = new UserServiceClient();
-    
     function main() {
-
-
         $("#loginBtn").click(login);
-        
     }
-    
+
     function login(e) {
         e.preventDefault();
         var username = $("#usernameFld").val();
         var password = $("#passwordFld").val();
 
-
-        var user = {
-          "username":username,
-          "password":password
-        };
-
-        userService
-            .login(user)
-            .then(function (response) {
-                if (response.status == '404'){
-                    alert("Incorrect Username or Password ");
-                }
-                else{
-                    window.location.href = "../profile/profile.template.client.html";
-                }
-            });
-
-
+        if (username === "" || password === "") {
+            alert("All Fields are mandatory");
+        }
+        else {
+            var user = {
+                "username": username,
+                "password": password
+            };
+            userService
+                .login(user)
+                .then(function (response) {
+                    if (response.status == '404') {
+                        alert("Incorrect Username or Password ");
+                    }
+                    else {
+                        window.location.href = "../profile/profile.template.client.html";
+                    }
+                });
+        }
     }
 })();
