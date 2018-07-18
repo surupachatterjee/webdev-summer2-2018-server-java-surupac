981ae1aa958f027bbc1289686c1401def90c74a1
chatterjeesurupa <chatterjee.su@husky.neu.edu>
2018-07-18 01:08:20 -0400

    implemented lesson model,repository and service

---
+++ src/main/java/com/example/webdevsummer22018serverjavasurupac/repositories/LessonRepository.java
@@ -0,0 +1,10 @@
+package com.example.webdevsummer22018serverjavasurupac.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.webdevsummer22018serverjavasurupac.models.Lesson;
+
+
+public interface LessonRepository  extends CrudRepository<Lesson,Integer>{
+
+}
